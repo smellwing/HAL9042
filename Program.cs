26064d808bdf61cdaa4cc1d0539840fcc4abefde
Juan Esteban Valenzuela Rodríguez <juan.valenzuelar@sag.gob.cl>
2023-09-16 00:18:34 -0300

    - Uso de paquete nuget Owner avatar whetstone.chatgpt https://github.com/johniwasz/whetstone.chatgpt - Se hace la estructura básica de Progrma.cs - Se agrega la instancia de ChatGPT

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿using Whetstone.ChatGPT;
+using Whetstone.ChatGPT.Models;
+
+class Program
+{
+    
+
+    static async Task Main ( string[] args )
+
+    {
+        var chatHistory = new List<string> ();
+        var exitCommand = "/exit";
+        
+        Console.WriteLine ("¡Bienvenido al chat con ChatGPT!");
+        Console.WriteLine ("Escribe '/exit' para salir del chat.");
+
+    
+        while (true)
+        {
+            Console.Write ("Tú: ");
+            var userInput = Console.ReadLine ();
+
+            if (userInput is not null && userInput.Equals (exitCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                break;
+            }
+            using IChatGPTClient client = new ChatGPTClient ("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");
+
+            chatHistory.Add ($"Tú: {userInput}");
+
+            var gptRequest = new ChatGPTChatCompletionRequest ()
+            {
+                Model = ChatGPT35Models.Turbo,
+                Messages = new List<ChatGPTChatCompletionMessage> ()
+                {
+                    new ChatGPTChatCompletionMessage()
+                    {
+                        Role = MessageRole.User,
+                    }
+                },               
+            };
+            
+            var response = await client.CreateChatCompletionAsync (gptRequest);
+            var aiResponse = response?.GetCompletionText ();
+            chatHistory.Add ($"ChatGPT: {aiResponse}");
+            Console.WriteLine ($"ChatGPT: {aiResponse}");
+
+        }
+        Console.WriteLine ("¡Chat finalizado!");
+
+    }        
+}
