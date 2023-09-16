498a77aa1c054ee751f03ca2026fc82ba185a3ed
Juan Esteban Valenzuela Rodríguez <juan.valenzuelar@sag.gob.cl>
2023-09-16 09:23:43 -0300

    El sistema es funcional en su minima expresión

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
 ﻿using Whetstone.ChatGPT;
 using Whetstone.ChatGPT.Models;
 
+/// <summary>
+/// Clase principal del programa.
+/// </summary>
 class Program
 {
-    
 
+    /// <summary>
+    /// Método principal del programa.
+    /// </summary>
+    /// <param name="args">Argumentos de la línea de comandos.</param>
     static async Task Main ( string[] args )
-
     {
         var chatHistory = new List<string> ();
         var exitCommand = "/exit";
-        
+
         Console.WriteLine ("¡Bienvenido al chat con ChatGPT!");
         Console.WriteLine ("Escribe '/exit' para salir del chat.");
 
-    
         while (true)
         {
             Console.Write ("Tú: ");
@@ -24,10 +28,14 @@ class Program
             {
                 break;
             }
-            using IChatGPTClient client = new ChatGPTClient ("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");
+
+
+            // Inicializar el cliente de ChatGPT
+            using IChatGPTClient client = new ChatGPTClient ("YOUR_API_KEY");
 
             chatHistory.Add ($"Tú: {userInput}");
 
+            // Acá instanciamos el objeto de respuesta
             var gptRequest = new ChatGPTChatCompletionRequest ()
             {
                 Model = ChatGPT35Models.Turbo,
@@ -36,17 +44,19 @@ class Program
                     new ChatGPTChatCompletionMessage()
                     {
                         Role = MessageRole.User,
+                        Content = userInput // Entrada del usuario
                     }
-                },               
+                },
+                // # Tokens = $ Stream = Respuestas largas
+                MaxTokens = 100
             };
-            
+
             var response = await client.CreateChatCompletionAsync (gptRequest);
             var aiResponse = response?.GetCompletionText ();
             chatHistory.Add ($"ChatGPT: {aiResponse}");
             Console.WriteLine ($"ChatGPT: {aiResponse}");
-
         }
-        Console.WriteLine ("¡Chat finalizado!");
 
-    }        
+        Console.WriteLine ("¡Chat finalizado!");
+    }
 }
